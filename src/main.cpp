b9dc82ce0b51cf58ee0e98b1e20f16fa0f28453c
Gergő Tóth <gergocsige@proton.me>
2026-02-22 20:33:44 +0100

    Added a basic moving average smoothing.

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,44 @@ struct Point {
 
 Point operator+( const Point& p1, const Point& p2) { return {p1.x + p2.x, p1.y + p2.y}; }
 Point operator+( const Point& p1, double c) { return {p1.x + c, p1.y + c}; }
+Point operator/( const Point& p1, double d) { return {p1.x / d, p1.y / d}; }
 
-void smooth_moving_avg(vector<Point>& points);
+void smooth_moving_avg(const vector<Point>& jagged, vector<Point>& smooth) {
+    int len = jagged.size();
+    if (len < 3) {
+        for( int i = 0; i < len; ++i ) {
+            smooth.at(i) = jagged.at(i);
+        } return;
+    } else {
+        smooth.at(0) = jagged.at(0);
+        for( int i = 1; i < len - 1; ++i ) {
+            smooth.at(i) = ( jagged.at(i-1) + jagged.at(i) + jagged.at(i+1) ) / 3;
+        }
+        smooth.at(len-1) = jagged.at(len-1);
+    }
+}
 
 int main(void) {
     int n;
     cin >> n;
     vector<Point> points(n);
+    vector<Point> smooth(n);
+
+    for(Point& pt : points) {
+        cout << "Enter Point x coordinate: ";
+        cin >> pt.x;
+        cout << "Enter Point y coordinate: ";
+        cin >> pt.y;
+    }
 
     for(Point pt : points) {
-        pt = {1.0, 1.0};
-        cout << pt.x << pt.y << endl;
+        cout << pt.x << " " << pt.y << endl;
+    }
+
+    smooth_moving_avg(points, smooth);
+
+    for(Point pt : smooth) {
+        cout << pt.x << " " << pt.y << endl;
     }
 
     return 0;
