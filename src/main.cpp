007ae427e170f49f6c9cc60cdec2012e3860b19f
Gergő Tóth <gergocsige@proton.me>
2026-02-22 16:57:03 +0100

    Started testing a point vector based approach (better for safety than dynamic arrays)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Point {
@@ -14,6 +15,17 @@ struct Point {
 Point operator+( const Point& p1, const Point& p2) { return {p1.x + p2.x, p1.y + p2.y}; }
 Point operator+( const Point& p1, double c) { return {p1.x + c, p1.y + c}; }
 
+void smooth_moving_avg(vector<Point>& points);
+
 int main(void) {
+    int n;
+    cin >> n;
+    vector<Point> points(n);
+
+    for(Point pt : points) {
+        pt = {1.0, 1.0};
+        cout << pt.x << pt.y << endl;
+    }
+
     return 0;
 }
